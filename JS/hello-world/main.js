b0e15c2e34da75be4fe5ecd038e5ed76365e62ef
Lucas <lsantos2003silva@gmail.com>
2023-12-20 15:37:40 -0300

    testing JavaScript

---
+++ JS/hello-world/main.js
@@ -0,0 +1,71 @@
+// console.log("Hello, World!");
+
+// const pessoa = {
+//     nome : "lucas",
+//     sobrenome : "santos",
+//     idade : 20
+// };
+
+// const str = `Meu nome é ${pessoa.nome} ${pessoa.sobrenome}, e tenho ${pessoa.idade} anos.`;
+
+// console.log(str);
+
+// /* conversion */
+// console.log(String(pessoa.idade), pessoa.idade);
+// console.log(typeof(String(pessoa.idade)));
+// console.log(typeof(pessoa.idade));
+
+// /* coersion */
+// console.log(`idade: ${pessoa.idade}`);
+// console.log("altura: " + 1.94);
+// console.log("23" - "10" - 3); 
+// console.log("23" + "10" + 3);
+// console.log("23" * "2");
+// console.log("23" / "2");
+
+// /* true or false */
+// console.log(Boolean(0));
+// console.log(Boolean(''));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+// if (pessoa.nome) {
+//     console.log("pessoa is defined");
+// } else {
+//     console.log("pessoa is undefined");
+
+// const scoreDolphins = [96, 108, 89];
+// const scoreKoalas = [88, 91, 110];
+
+// let avgDol;
+// let avgKoa;
+
+
+// for (const key in scoreDolphins)
+//     avgDol += scoreDolphins[key];
+
+// for (const key in scoreKoalas)
+//     avgKoa += scoreKoalas[key];
+
+// avgDol = avgDol / scoreDolphins.length;
+// avgKoa = avgKoa / scoreKoalas.length;
+
+// if (avgDol === avgKoa) 
+//     console.log("Both win the trophy");
+// else if (avgDol > avgKoa) 
+//     console.log("Dolphins win the trophy");
+// else 
+//     console.log("Koalas win the trophy");
+
+
+// const bill = 275;
+
+/* Write your code below. Good luck! 🙂 */
+
+function sum (x, y)
+{
+    return x + y;
+}
+
+console.log(sum(41, 23));
