8fdc5f6cf5c8e68916354ef78561828053efc4dc
Lucas <lsantos2003silva@gmail.com>
2024-02-18 00:11:12 -0300

    Update celsius.d

--- rust/fundamentals/celsius/target/debug/celsius.d
+++ rust/fundamentals/celsius/target/debug/celsius.d
@@ -1,1 +1,1 @@
-/run/host/media/dio/code/git\ repositories/just-coding/rust/celsius/target/debug/celsius: /run/host/media/dio/code/git\ repositories/just-coding/rust/celsius/src/main.rs
+/run/host/media/dio/code/git\ repositories/just-coding/rust/fundamentals/celsius/target/debug/celsius: /run/host/media/dio/code/git\ repositories/just-coding/rust/fundamentals/celsius/src/main.rs
