333f21a42a6f718c24c8810b725ec506ff466ee1
Lucas Santos <lsantos2003silva@gmail.com>
2024-06-07 16:17:36 -0300

    adding my python file-manager

--- py/showing-files/main.py
+++ py/showing-files/main.py
@@ -1,16 +1,13 @@
-def busca_binaria(v, key):
-    m = (len(v) - 1) // 2
-    if m >= 0:  
-        if key < v[m]:
-            return busca_binaria(v[:m], key)
-        elif key > v[m]:
-            return busca_binaria(v[m + 1:], key)
-        elif key == v[m]:
-            return v[m]
-    else:
-        return None
+import os
+
+# getting the current directory
+print(os.getcwd())
+
+# getting the directorys in the same folder
+print(os.listdir())
+
+# getting the os name (posix if you are on linux)
+print(os.name)
 
 
-l = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
-print(busca_binaria(l, 9))
