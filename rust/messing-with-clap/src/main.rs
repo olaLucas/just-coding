c7a7a4b2b307e8306a7de05a2a77a2658c9ea576
Lucas <lsantos2003silva@gmail.com>
2024-06-23 02:47:36 -0300

    suffering with clap

---
+++ rust/messing-with-clap/src/main.rs
@@ -0,0 +1,54 @@
+use clap::*;
+
+fn sub(vec_numbers: &Vec<i32>) -> i32 {
+  let mut result: i32 = 0;
+  for i in vec_numbers {
+    result += i;
+  }
+
+  return result;
+}
+
+fn add(vec_numbers: &Vec<i32>) -> i32 {
+  let mut result: i32 = 0;
+  for i in vec_numbers {
+    result += i;
+  }
+
+  return result;
+}
+
+fn parse_terminal_args(arguments: &[Arg]) -> ArgMatches {
+
+  let matches = Command::new("TESTING CLAP")
+  .author("me, myself and I")
+  .about("testing how to handle cli args with clap")
+  .args(arguments)
+  .get_matches();
+
+
+  println!("{:#?}", matches);
+  return matches;
+}
+
+
+fn main() {
+  let arguments: &[Arg; 2] = &[
+    arg!(sum: -a --sum [NUMBERS] "sum two numbers")
+      .value_parser(value_parser!(i32))
+      .num_args(1..),
+    arg!(sub: -s --sub [NUMBERS] "subtract two numbers")
+      .value_parser(value_parser!(i32))
+      .num_args(1..),
+  ];
+
+  let matches = parse_terminal_args(arguments);
+  if let Some(args) = matches.get_many::<i32>("sum") {
+    let numbers = args.copied().collect();
+    println!("sum: {}", add(&numbers));
+  } 
+  else if let Some(args) = matches.get_many::<i32>("sub") {
+    let numbers = args.copied().collect();
+    println!("subtraction: {}", sub(&numbers));
+  }
+}
