21393022ff45702edbfcb2cb4d19d3bdc5e02901
Lucas <lsantos2003silva@gmail.com>
2024-06-25 23:58:57 -0300

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs

--- rust/messing-with-maps/src/main.rs
+++ rust/messing-with-maps/src/main.rs
@@ -1,49 +1,92 @@
-use std::collections::HashMap;
-use std::io::{ Error, ErrorKind };
+use walkdir::WalkDir;
 use std::path::Path;
-use std::fs::{ File, read_dir };
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::io;
 
-fn craw_directory(src: &Path) -> Result<(), Error> {
-  if src.exists() {
-    let mut stack: Vec<String> = Vec::new();
-    let mut visited: Vec<String> = Vec::new(); // TODO
+fn scan_dir_filename(path: &Path) -> Result <Option <Vec<String>>, io::Error> {
+  let mut vec: Vec <String> = Vec::new();
+  let entries = fs::read_dir(path)?;
+  for entry in entries {
+    vec.push( match entry {
+      Ok(f) => match f.path().file_name() {
+        Some(s) => s
+          .to_str()
+          .unwrap_or(&String::new())
+          .to_string(),
+        None => String::new(),
+      },
 
-    let src = src.to_str().unwrap();
-    stack.push(src.to_string());
-
-    // TODO: add identation (like tree)
-    // TODO return a hasmap with the directories
-    // and its files
-
-    while let Some(dir) = stack.pop() {
-      for entry in read_dir(dir)? {
-        
-        let entry = entry?;
-        let entry_path = entry.path();
-        let entry_str = match entry_path.to_str() {
-          Some(s) => s,
-          None => continue,
-        };
-
-
-        if entry.path().is_dir() {
-          
-          stack.push(entry_str.to_string());
-          println!("dir: {}", entry_str);
-
-        } else {
-          // println!("file: {}", entry_str);
-        }
-      }
-    }  
-  } else {
-    return Err(Error::from(ErrorKind::NotFound));
+      Err(e) => { eprintln!("error: {:#?}", e); String::new() },
+    });
   }
 
-  Ok(())
+  Ok(Some(vec))
 }
 
+fn scan_dir(path: &Path) -> Result <Option <Vec<String>>, io::Error> {
+  let mut vec: Vec <String> = Vec::new();
+  let entries = fs::read_dir(path)?;
+  for entry in entries {
+    vec.push(match entry {
+      Ok(f) => match f.path().to_str() {
+        Some(s) => String::from(s),
+        None => String::new(),
+      },
+
+      Err(e) => { eprintln!("error: {:#?}", e); String::new() },
+    });
+  }
+
+  Ok(Some(vec))
+}
+
+fn craw_dir(path: &Path) -> Result< Option <HashMap <String, Vec<String>>>, walkdir::Error> {
+  let mut map: HashMap <String, Vec<String>> = HashMap::new();
+
+  if path.exists() {
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+      
+      if entry.path().is_dir() {
+        let mut path: Vec<&Path> = entry
+          .path()
+          .ancestors()
+          .collect();
+        path.reverse();
+
+        for dir in path {
+          let path_str: &str = match dir.to_str() {
+            Some(s) => s,
+            None => &String::new(),
+          };
+
+          if !map.contains_key(path_str) {
+            map.insert(path_str.to_string(), match scan_dir(&Path::new(path_str)) {
+              Ok(s) => match s {
+                Some(vec) => vec,
+                None => Vec::new()
+              },
+
+              Err(e) => { eprintln!("error: {:#?}", e); Vec::new()}, 
+            });
+
+          } else {
+            continue;
+          }
+        }
+      }
+    }
+  }
+
+  Ok(Some(map))
+}
 
 fn main() {
-  craw_directory(Path::new("/home/dio/git_repos/")).unwrap();
+  let path = env::args().last().unwrap();
+  match craw_dir(Path::new(&path)) {
+    Ok(Some(map)) => println!("{:#?}", map),
+    Ok(None) => println!("craw_dir returned None."),
+    Err(e) => eprintln!("vish, deu erro: {:#?}", e),
+  }
 }
