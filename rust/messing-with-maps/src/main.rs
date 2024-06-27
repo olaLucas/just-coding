a4cebf007dc91975ccf639e97530154ba3de7d21
Lucas <lsantos2003silva@gmail.com>
2024-06-26 23:14:15 -0300

    making clippy happy

--- rust/messing-with-maps/src/main.rs
+++ rust/messing-with-maps/src/main.rs
@@ -10,15 +10,14 @@ fn scan_dir_filename(path: &Path) -> Result <Option <Vec<String>>, io::Error> {
   let entries = fs::read_dir(path)?;
   for entry in entries {
     vec.push( match entry {
-      Ok(f) => match f.path().file_name() {
-        Some(s) => s
-          .to_str()
-          .unwrap_or(&String::new())
-          .to_string(),
-        None => String::new(),
-      },
+      Ok(f) => f.path()
+        .file_name()
+        .map_or_else(String::new, 
+          |s| s.to_str()
+          .unwrap_or("")
+          .to_string()),
 
-      Err(e) => { eprintln!("error: {:#?}", e); String::new() },
+      Err(e) => { eprintln!("error: {e:#?}"); String::new() },
     });
   }
 
@@ -30,12 +29,11 @@ fn scan_dir(path: &Path) -> Result <Option <Vec<String>>, io::Error> {
   let entries = fs::read_dir(path)?;
   for entry in entries {
     vec.push(match entry {
-      Ok(f) => match f.path().to_str() {
-        Some(s) => String::from(s),
-        None => String::new(),
-      },
+      Ok(f) => f.path()
+        .to_str()
+        .map_or_else(String::new, String::from),
 
-      Err(e) => { eprintln!("error: {:#?}", e); String::new() },
+      Err(e) => { eprintln!("error: {e:#?}"); String::new() },
     });
   }
 
@@ -46,34 +44,21 @@ fn craw_dir(path: &Path) -> Result< Option <HashMap <String, Vec<String>>>, walk
   let mut map: HashMap <String, Vec<String>> = HashMap::new();
 
   if path.exists() {
-    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-      
+    for entry in WalkDir::new(path).into_iter().flatten() {
       if entry.path().is_dir() {
-        let mut path: Vec<&Path> = entry
-          .path()
-          .ancestors()
-          .collect();
-        path.reverse();
+        let path: &Path = entry.path();
+        let path_str: &str = path
+          .to_str()
+          .unwrap_or("");
 
-        for dir in path {
-          let path_str: &str = match dir.to_str() {
-            Some(s) => s,
-            None => &String::new(),
-          };
-
-          if !map.contains_key(path_str) {
-            map.insert(path_str.to_string(), match scan_dir(&Path::new(path_str)) {
-              Ok(s) => match s {
-                Some(vec) => vec,
-                None => Vec::new()
-              },
-
-              Err(e) => { eprintln!("error: {:#?}", e); Vec::new()}, 
-            });
-
-          } else {
-            continue;
-          }
+        if !map.contains_key(path_str) {
+          map.insert(path_str.to_string(), match scan_dir(Path::new(path_str)) {
+            Ok(s) => s.unwrap_or_else(Vec::new),
+            Err(e) => { eprintln!("error: {e:#?}"); Vec::new()}, 
+          });
+          
+        } else {
+          continue;
         }
       }
     }
@@ -83,10 +68,10 @@ fn craw_dir(path: &Path) -> Result< Option <HashMap <String, Vec<String>>>, walk
 }
 
 fn main() {
-  let path = env::args().last().unwrap();
-  match craw_dir(Path::new(&path)) {
-    Ok(Some(map)) => println!("{:#?}", map),
+  let arg = env::args().last().unwrap();
+  match craw_dir(Path::new(&arg)) {
+    Ok(Some(map)) => println!("{map:#?}"),
     Ok(None) => println!("craw_dir returned None."),
-    Err(e) => eprintln!("vish, deu erro: {:#?}", e),
+    Err(e) => eprintln!("vish, deu erro: {e:#?}"),
   }
 }
