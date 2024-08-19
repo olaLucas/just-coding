845834362e97dcd8978959c4e1b663dd091d38ed
Lucas Santos <lsantos2003silva@gmail.com>
2024-08-19 07:59:06 +0000

    modified:   src/main.rs 	new file:   src/tui.rs

--- rust/messing-with-ratatui/src/main.rs
+++ rust/messing-with-ratatui/src/main.rs
@@ -1,55 +1,12 @@
-use std::io::stdout;
-use std::time::Duration;
+use ratatui as rat;
 
-use ratatui::{
-    backend::CrosstermBackend,
-    crossterm::{
-        terminal::{
-            enable_raw_mode,
-            disable_raw_mode,
-            EnterAlternateScreen,
-            LeaveAlternateScreen,
-        },
-        ExecutableCommand,
-        event::{
-            self,
-            KeyCode,
-            KeyEventKind,
-        },
-    },
-
-
-    widgets::Paragraph,
-    Terminal,
-};
+mod tui;
 
 fn main() {
-    stdout().execute(EnterAlternateScreen).unwrap();
-    enable_raw_mode().unwrap();
+    tui::init().unwrap();
+    let mut a = tui::App::new();
+    let mut term = rat::Terminal::new(rat::backend::CrosstermBackend::new(std::io::stdout())).unwrap();
+    let _ = a.run(&mut term);
 
-    let mut term = Terminal::new(
-        CrosstermBackend::new(stdout())
-    ).unwrap();
-
-    term.clear().unwrap();
-
-    loop {
-        term.draw(|frame| {
-            frame.render_widget(
-                Paragraph::new("Hello, world! (presss 'q' to quit)."),
-                frame.area()
-            )
-        }).unwrap();
-
-        if event::poll(Duration::from_millis(16)).unwrap() {
-            if let event::Event::Key(key) = event::read().unwrap() {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
-                }
-            }
-        }
-    }
-
-    disable_raw_mode().unwrap();
-    stdout().execute(LeaveAlternateScreen).unwrap();
+    tui::restore().unwrap();
 }
---
+++ rust/messing-with-ratatui/src/tui.rs
@@ -0,0 +1,110 @@
+use std::io;
+use ratatui::{self as rat, style::Stylize, widgets::Paragraph};
+
+pub struct App {
+    counter: i8,
+    exit: bool
+}
+
+impl App {
+    
+    pub fn new() -> App {
+        App::default()
+    }
+
+    pub fn default() -> App {
+        App { counter: 0, exit: false }
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter = self.counter - 1;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter = self.counter + 1;
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    pub fn run(&mut self, terminal: &mut Tui) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+
+        Ok(())
+    }
+
+    fn render_frame(&self, frame: &mut rat::Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match rat::crossterm::event::read()? {
+            rat::crossterm::event::Event::Key(k) if k.kind == rat::crossterm::event::KeyEventKind::Press => {
+                self.handle_key_events(k)
+            },
+
+            _ => {}
+        }
+
+        Ok(()) 
+    }
+
+    fn handle_key_events(&mut self, k: rat::crossterm::event::KeyEvent) {
+        match k.code {
+            rat::crossterm::event::KeyCode::Char('q') => self.exit(),
+            rat::crossterm::event::KeyCode::Left => self.decrement_counter(),
+            rat::crossterm::event::KeyCode::Right => self.increment_counter(),
+            _ => {},
+        }
+    }
+}
+
+impl rat::widgets::Widget for &App {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
+        let title = rat::widgets::block::Title::from(" Counting App ".bold());
+        let instructions = rat::widgets::block::Title::from(rat::text::Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+
+        let block = rat::widgets::Block::bordered()
+            .title(title.alignment(rat::layout::Alignment::Center))
+            .title(
+                instructions
+                    .alignment(rat::layout::Alignment::Center)
+                    .position(rat::widgets::block::Position::Bottom))
+            .border_set(rat::symbols::border::THICK);
+
+        let center_text = rat::text::Text::from(vec![rat::text::Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(center_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    } 
+}
+
+pub type Tui = rat::Terminal<rat::backend::CrosstermBackend<io::Stdout>>;
+
+pub fn init() -> io::Result<Tui> {
+    rat::crossterm::execute!(io::stdout(), rat::crossterm::terminal::EnterAlternateScreen)?;
+    rat::crossterm::terminal::enable_raw_mode()?;
+    rat::Terminal::new(rat::backend::CrosstermBackend::new(io::stdout()))
+}
+
+pub fn restore() -> io::Result<()> {
+    rat::crossterm::execute!(io::stdout(), rat::crossterm::terminal::LeaveAlternateScreen)?;
+    rat::crossterm::terminal::disable_raw_mode()?;
+    Ok(())
+}
